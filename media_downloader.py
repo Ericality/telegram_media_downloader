b311bf87109f4e3c52b9d79cf8282f6f747997de
“Ericality” <>
2025-12-28 09:26:47 +0800

    add download dir size in norification

--- media_downloader.py
+++ media_downloader.py
@@ -10,6 +10,7 @@ import sys
 import time
 from datetime import datetime, timedelta
 from typing import List, Optional, Tuple, Union, Dict, Any, Callable
+from pathlib import Path
 
 import aiohttp
 import psutil
